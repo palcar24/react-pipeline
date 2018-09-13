b5a6bf65d819382897987e86dbabf9b0af682cf4
Hayes <hayes@Upstairs-iMac.frontierlocal.net>
2018-09-13 15:21:05 -0500

    Logo Change

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -38,6 +38,7 @@ class Sidebar extends Component{
                             </div>
 
                         </a>
+                        
                         <a href="/" className="simple-text logo-normal">
                             React Pipeline
                         </a>
